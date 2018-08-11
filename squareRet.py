898e9ef31b26f459a6dffe12c53709961d4bd655
dunsha-mha <morennoalves@hotmail.com>
2018-08-11 16:00:16 -0400

    Drawing a square, circle and a triangle.

--- squareRet.py
+++ squareRet.py
@@ -9,18 +9,51 @@ def draw_square():
     turtle.shapesize()
     (50,50,50)
     moe.color("red","white")
-    moe.speed(1)
+    moe.speed(10)
    
-    
-    moe.forward(100)
-    moe.left(90)
-    moe.forward(100)
-    moe.left(90)
-    moe.forward(100)
-    moe.left(90)
-    moe.forward(100)
-    moe.left(90)
-    
-    window.exitonclick()
+    line = 1
+    square = 4
+
+    while line <= square: 
+        moe.forward(400)
+        moe.left(90)
+
+        line = line + 1
     
 draw_square()
+
+def draw_circle():
+   
+    sarah = turtle.Turtle()
+    sarah.shape("turtle")
+    turtle.shapesize()
+    (60,60,60)
+    sarah.color("blue")
+    sarah.speed(10)
+   
+    sarah.forward(200)
+    sarah.circle(199)
+
+draw_circle()
+
+def draw_triangle():
+
+    right_tri = turtle.Turtle()
+    right_tri.shape("arrow")
+    right_tri.shapesize()
+    (60,60,60)
+    right_tri.color("green","white")
+    right_tri.speed(10)
+    right_tri.left(45)
+    right_tri.forward(85)
+    right_tri.left(23)
+    right_tri.forward(363)
+    right_tri.left(225)
+    right_tri.forward(370)
+    right_tri.right(113)
+    right_tri.forward(282)
+
+
+draw_triangle()
+
+window.exitonclick()
